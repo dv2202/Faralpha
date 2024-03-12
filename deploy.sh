560083647b5d1ebca7f4fba5d0c7458d366bce38
Devansh Vishwakarma <devwish0207@gmail.com>
2024-03-12 13:28:11 +0530

    github action

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Activate virtual environment (assuming it's already created)
+# Activate virtual environment
 source venv/bin/activate
 
 # Install Python dependencies
 pip install -r requirements.txt
 
-# Restart FastAPI application (replace 'your_command' with the actual command)
-supervisorctl restart your_command
+# Restart FastAPI application
+supervisorctl restart fastApiProject
